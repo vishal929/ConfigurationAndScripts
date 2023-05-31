6ce2cfe96211b46b4ade166b16dc31565036caed
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-31 15:40:16 -0400

    init powershell script

---
+++ Powershell/incremented_rename.ps1
@@ -0,0 +1,5 @@
+# This is a script to help manage show files after dumping disks
+# basically in a Season Directory we might have inconsistent filenames
+# this script takes as input a basename i.e "S01".
+# then this sorts all the files in the directory by time modified (assuming that the write time is consistent with the actual episode order)
+# then the files in the directory are renamed something like "S01-E01", "S01-E02", and so on...
